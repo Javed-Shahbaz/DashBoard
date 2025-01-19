da7ecae0e38d946c02f0bbf43b262cf144b4e063
Javed-Shahbaz <javedshahbaz007@gmail.com>
2025-01-19 20:58:44 +0500

    update authController

--- SERVER/controller/authController.js
+++ SERVER/controller/authController.js
@@ -1,3 +1,7 @@
-const login = async (req, res) => {};
-
-export default login;
+if (!process.env.JWT_KEY) {
+  console.error("JWT_KEY is not defined in environment variables.");
+  res
+    .status(500)
+    .json({ success: false, error: "JWT key not configured in server" });
+  return;
+}
